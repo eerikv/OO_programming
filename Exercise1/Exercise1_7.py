a6d353c49c6a4c4ca2736294f9ba059c7a7987e6
Eerik <eerik@beanbakers.fi>
2024-01-12 15:58:30 +0200

    commit

---
+++ Exercise1/Exercise1_7.py
@@ -0,0 +1,16 @@
+# File name:    Exercise1_7.py
+# Author:       Eerik Vainio
+# Description:  
+
+max_value, number_of_terms, sum_of_terms, sum_of_sq_terms = 0
+
+while(True):
+    user_input = input("Give the maximum value: ")
+    if(input.isnumeric()):
+        break
+    else:
+        print(f'"{user_input}" is not a valid value.')
+
+
+
+
